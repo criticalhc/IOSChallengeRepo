d0a44c7a6e1087a94799919f1963b8299d44f7d3
Heydon Costello <heydonc@icloud.con>
2022-05-17 13:41:34 +0100

    Initial Commit

---
+++ IOSChallenge/IOSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSChallengeApp.swift
+//  IOSChallenge
+//
+//  Created by Heydon Costello on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
